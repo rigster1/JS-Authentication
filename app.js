6e7cac8f1bfc8d9bbacda45228bfbee55125beb7
Scott <scott.taylor485@gmail.com>
2020-11-15 15:54:07 +0000

    Add salting

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -39,18 +40,25 @@ app.get("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
+
   User.findOne({email: username}, function(err, foundUser) {
     if (err) {
       console.log(err);
       res.send(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        } else {
-          res.send("Invalid credentials");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, samePassword) {
+          if (err) {
+            res.send(err);
+          } else{
+            if (samePassword === true) {
+              res.render("secrets");
+            } else {
+              res.send("Invalid credentials");
+            }
+          }
+        });
       } else {
         res.send("Invalid credentials");
       }
@@ -59,18 +67,21 @@ app.post("/login", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
 
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err);
-      res.send(err);
-    } else {
-      res.render("secrets");
-    }
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+        res.send(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
